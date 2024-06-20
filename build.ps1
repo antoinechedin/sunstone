87d6d2e28e077399ec8009ca47620eae8b492556
antoinechedin <antoine.chedin@gmail.com>
2024-06-20 16:34:05 +0200

    Handle alpha value for named colors

--- build.ps1
+++ build.ps1
@@ -7,13 +7,23 @@ param(
 $Colors = Get-Content $Palette | Out-String | ConvertFrom-Json
 $Theme = Get-Content $Template | Out-String
 
-$Results = $Theme | Select-String ": *\""(.*?)\""" -AllMatches 
+$Results = $Theme | Select-String ": *\""(.*?)(?:@([0-9]+[.]?[0-9]*))?\""" -AllMatches 
 
 # Replace named color found in the palette
 for ($i = $Results.Matches.Count - 1; $i -ge 0; $i--) {
     $Match = $Results.Matches[$i].Groups[1]
+    
     if ($Match.Value -in $Colors.PSobject.Properties.Name) {
-        $Theme = $Theme.Remove($Match.Index, $Match.Length).Insert($Match.Index, $Colors.$($Match.Value))
+        $Color = $Colors.$($Match.Value)
+        $MatchLength = $Match.Length
+        
+        if ($Results.Matches[$i].Groups[2].Success) {
+            $MatchLength += $Results.Matches[$i].Groups[2].Length + 1
+            $Alpha = [double] $Results.Matches[$i].Groups[2].Value
+            $Color += "{0:x2}" -f  $([int][math]::Round($Alpha * 255))
+        }
+    
+        $Theme = $Theme.Remove($Match.Index, $MatchLength).Insert($Match.Index, $Color)
     }
 }
 
