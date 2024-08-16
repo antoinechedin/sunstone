e6b14c9e0abc973e0bf8e9737a2f73ea5e423af6
antoinechedin <antoine.chedin@gmail.com>
2024-08-16 16:58:01 +0200

    add theme with light header

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,35 +1,43 @@
 param(
-    [string]$Template = "$PSScriptRoot\..\themes\sunstone-color-template.json",
-    [string]$Palette = "$PSScriptRoot\..\themes\palette.json",
-    [string]$BuildDir = "$PSScriptRoot\..\build"
+    [string]$Src = "$PSScriptRoot\..\themes",
+    [string]$Build = "$PSScriptRoot\..\build",
+    [string]$PaletteFileName = "palette.json"
 )
 
-$Colors = Get-Content $Palette | Out-String | ConvertFrom-Json
-$Theme = Get-Content $Template | Out-String
+$Colors = Get-Content $(Join-Path $Src $PaletteFileName) | Out-String | ConvertFrom-Json
+$Files = Get-ChildItem $Src 
 
-$Results = $Theme | Select-String ": *\""(.*?)(?:@([0-9]+[.]?[0-9]*))?\""" -AllMatches 
-
-# Replace named color found in the palette
-for ($i = $Results.Matches.Count - 1; $i -ge 0; $i--) {
-    $Match = $Results.Matches[$i].Groups[1]
-    
-    if ($Match.Value -in $Colors.PSobject.Properties.Name) {
-        $Color = $Colors.$($Match.Value)
-        $MatchLength = $Match.Length
-        
-        if ($Results.Matches[$i].Groups[2].Success) {
-            $MatchLength += $Results.Matches[$i].Groups[2].Length + 1
-            $Alpha = [double] $Results.Matches[$i].Groups[2].Value
-            $Color += "{0:x2}" -f  $([int][math]::Round($Alpha * 255))
-        }
-    
-        $Theme = $Theme.Remove($Match.Index, $MatchLength).Insert($Match.Index, $Color)
+foreach($File in $Files) {
+    if($File.Name -eq $PaletteFileName) {
+        continue
     }
-}
 
-$OutFilePath = Join-Path $BuildDir sunstone-color-theme.json
-if (!(Test-Path $OutFilePath)) {
-    New-Item -Path $OutFilePath -ItemType File -Force
-}
+    $Theme = Get-Content $File.FullName | Out-String
+    
+    $Results = $Theme | Select-String ": *\""(.*?)(?:@([0-9]+[.]?[0-9]*))?\""" -AllMatches 
+    # Replace named color found in the palette
+    for ($i = $Results.Matches.Count - 1; $i -ge 0; $i--) {
+        $Match = $Results.Matches[$i].Groups[1]
+        
+        if ($Match.Value -in $Colors.PSobject.Properties.Name) {
+            $Color = $Colors.$($Match.Value)
+            $MatchLength = $Match.Length
+            
+            if ($Results.Matches[$i].Groups[2].Success) {
+                $MatchLength += $Results.Matches[$i].Groups[2].Length + 1
+                $Alpha = [double] $Results.Matches[$i].Groups[2].Value
+                $Color += "{0:x2}" -f  $([int][math]::Round($Alpha * 255))
+            }
+        
+            $Theme = $Theme.Remove($Match.Index, $MatchLength).Insert($Match.Index, $Color)
+        }
+    }
 
-$Theme | Out-File -FilePath $OutFilePath -Encoding utf8 -Force
+    $OutFile = $File.Name.Replace("color-template.json", "color-theme.json")
+    $OutFilePath = Join-Path $Build $OutFile
+    if (!(Test-Path $OutFilePath)) {
+        New-Item -Path $OutFilePath -ItemType File -Force
+    }
+
+    $Theme | Out-File -FilePath $OutFilePath -Encoding utf8 -Force
+}
