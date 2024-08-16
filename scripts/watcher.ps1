85cbe886cfff04f44d37927665a18d5900617518
antoinechedin <antoine.chedin@gmail.com>
2024-08-16 10:34:58 +0200

    Fix watcher script

--- scripts/watcher.ps1
+++ scripts/watcher.ps1
@@ -3,12 +3,12 @@ Push-Location "$PSScriptRoot\.."
 $OnChangedEvent = $null
 try {
     $Watcher = New-Object IO.FileSystemWatcher ".\themes", "*.*" -Property @{ 
-        IncludeSubdirectories = $false
+        IncludeSubdirectories = $true
         EnableRaisingEvents   = $true
     }
 
     $OnChangedEvent = Register-ObjectEvent $Watcher Changed -Action {
-        .\build.ps1
+        .\scripts\build.ps1
     }
 
     while ($true) {
